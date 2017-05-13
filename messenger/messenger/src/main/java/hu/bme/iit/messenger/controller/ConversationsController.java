8c350784fe10d37c877b62de876a485c160f47d1
guitarrespieler <tibor.zsiga@hotmail.com>
2017-05-14 01:14:54 +0200

    asdf

--- messenger/messenger/src/main/java/hu/bme/iit/messenger/controller/ConversationsController.java
+++ messenger/messenger/src/main/java/hu/bme/iit/messenger/controller/ConversationsController.java
@@ -1,5 +1,7 @@
 package hu.bme.iit.messenger.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import hu.bme.iit.messenger.controller.services.ConversationService;
 import hu.bme.iit.messenger.controller.services.UserService;
+import hu.bme.iit.messenger.model.Conversation;
 import hu.bme.iit.messenger.model.User;
 
 @RestController
@@ -23,14 +27,32 @@ public class ConversationsController {
 	@RequestMapping(value = convlist, method=RequestMethod.GET)
 	public String getConversationsList(HttpSession session){
 		if(!LoginController.isLoggedIn(session))
-			return "redirect: /login";
+			return "{\"url\":\""+LoginController.loginPage+"\"}";
 		try{
 			User user = (User)session.getAttribute(LoginController.userSessionAttribName);
+			
+			return createConversationList(user.getConversations());
 		}catch (Exception e) {
 			return "{\"error\": \"Something went wrong. Try again later.\"}";
-		}
+		}		
+	}
+
+
+	private String createConversationList(List<Conversation> conversations) {
+		StringBuilder sb = new StringBuilder();
 		
-		return "conversations";
+		sb.append("{\"conversations\":[");
+		
+		if(conversations != null ){
+			for (Conversation conversation : conversations) {
+				
+			}
+		}
+			
+		
+
+		sb.append("]}");
+		return sb.toString();
 	}
 	
 }
