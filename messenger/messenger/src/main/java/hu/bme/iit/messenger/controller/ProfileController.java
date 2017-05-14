3718586a7a29b286abd9a47c1b6cfe15ba82f93a
guitarrespieler <tibor.zsiga@hotmail.com>
2017-05-14 23:18:09 +0200

    profil kész!

--- messenger/messenger/src/main/java/hu/bme/iit/messenger/controller/ProfileController.java
+++ messenger/messenger/src/main/java/hu/bme/iit/messenger/controller/ProfileController.java
@@ -3,37 +3,53 @@ package hu.bme.iit.messenger.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import hu.bme.iit.messenger.controller.services.UserService;
 import hu.bme.iit.messenger.controller.services.UserService.ContactState;
 import hu.bme.iit.messenger.model.User;
 
+@RestController
 public class ProfileController {
 	
 	@Autowired
 	private UserService userService;
 
-	public static final String profilePage = "/profile";
+	public static final String profilePage = "/profiledata";
+	public static final String otherusersProfile = "/otherusersprofile";
 	
-	public String getProfile(@RequestParam("userid") Long userid, HttpSession session){
+	@RequestMapping(value = otherusersProfile, method = RequestMethod.GET)
+	public String getOtherUsersProfile(@RequestParam("userid") Long userid, HttpSession session){
 		if(!LoginController.isLoggedIn(session))
 			return "{\"url\":\""+LoginController.loginPage+"\"}";
 		try {
 			User loggedInUser = (User) session.getAttribute(LoginController.userSessionAttribName);
 			
-			if(userid != null){
-				User user = userService.getUser(userid);
-								
-				if(user == null || loggedInUser == null) throw new NullPointerException();
-				
-				ContactState contactstate = userService.areFriends(user, loggedInUser);
-				
-				return createJsonfromUser(user, contactstate);
-			}else{
-				return createJsonfromUser(loggedInUser, ContactState.SAME_USER);
-			}
+			User user = userService.getUser(userid);
+							
+			if(user == null || loggedInUser == null) throw new NullPointerException();
+			
+			ContactState contactstate = userService.areFriends(user, loggedInUser);
+			
+			return createJsonfromUser(user, contactstate);
+
 		} catch (Exception e) {
+			return "{\"error\":\"Something went wrong. Try again later.\"}";
+		}
+	}
+	@RequestMapping(value = profilePage, method = RequestMethod.GET)
+	public String getLoggedInUsersProfile(HttpSession session){
+		if(!LoginController.isLoggedIn(session))
+			return "{\"url\":\""+LoginController.loginPage+"\"}";
+		
+		try{
+			User loggedInUser = (User) session.getAttribute(LoginController.userSessionAttribName);
+			
+			return createJsonfromUser(loggedInUser, ContactState.SAME_USER);
+		}catch (Exception e) {
 			return "{\"error\":\"Something went wrong. Try again later.\"}";
 		}
 	}
@@ -44,19 +60,19 @@ public class ProfileController {
 		sb.append("{");
 		
 		sb.append("\"userid\":");
-		sb.append("\" "+ user.getId() + "\",");
+		sb.append("\""+ user.getId() + "\",");
 		
 		sb.append("\"firstname\":");
-		sb.append("\" "+ user.getFirstName() + "\",");
+		sb.append("\""+ user.getFirstName() + "\",");
 		
 		sb.append("\"lastname\":");
-		sb.append("\" "+ user.getLastName() + "\",");
+		sb.append("\""+ user.getLastName() + "\",");
 		
 		sb.append("\"city\":");
-		sb.append("\" "+ user.getCity() + "\",");
+		sb.append("\""+ user.getCity() + "\",");
 		
 		sb.append("\"birthdate\":");
-		sb.append("\" "+ user.getBirthDate() + "\",");
+		sb.append("\""+ user.getBirthDate() + "\",");
 
 		sb.append("\"contactstate\":");
 		sb.append("\"" + contactstate.name() + "\"");			
